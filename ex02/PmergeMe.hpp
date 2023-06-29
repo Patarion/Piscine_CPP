442fe6e6543c055e2509fd3ad162fb52739a241a
Patarion <62268532+Patarion@users.noreply.github.com>
2023-06-29 14:33:30 -0400

    Delete ex02 directory

--- ex02/PmergeMe.hpp
+++
@@ -1,8 +0,0 @@
-#include <list>
-#include <vector>
-#include <iostream>
-#include <iomanip>
-#include <ctime>
-
-void    Split_Vector(std::vector<int> *tab);
-void    Split_List(std::list<int> *tab);
